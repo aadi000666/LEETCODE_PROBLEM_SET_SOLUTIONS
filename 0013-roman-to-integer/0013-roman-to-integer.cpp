a487b4ad8c3146da58ab5962dea55faaed72cb14
Aditya Maurya <199570312+aadi000666@users.noreply.github.com>
2026-07-09 00:08:50 +0530

    Time: 7 ms (51.87%), Space: 13.4 MB (17.99%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <unordered_map>
+
+class Solution {
+public:
+    int romanToInt(std::string s) {
+        std::unordered_map<char, int> roman_map = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+        
+        int total = 0;
+        int prev_value = 0;
+        
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int current_value = roman_map[s[i]];
+            
+            if (current_value < prev_value) {
+                total -= current_value;
+            } else {
+                total += current_value;
+            }
+            
+            prev_value = current_value;
+        }
+        
+        return total;
+    }
+};
